66891e70aec48d995f425442e5066868a04a55f0
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-10 15:43:47 -0400

    first three

--- js/index.js
+++ js/index.js
@@ -1,22 +1,40 @@
 // Your code goes here
 
-homelink.addEvenListener(
-    'click',
-    () => {
+// homelink.addEvenListener(
+//     'click',
+//     () => {
 
-        console.log('the link got clicked');
-    }
-)
+//         console.log('the link got clicked');
+//     }
+// )
 
-document.querySelector('nav').addEventListener('click', () => {
-    console.log('the nav got clicked');
-})
+// document.querySelector('nav').addEventListener('click', () => {
+//     console.log('the nav got clicked');
+// })
 
-document.querySelector('header').addEventListener('click', () => {
-    console.log('Theheader got clicked');
-})
+// document.querySelector('header').addEventListener('click', () => {
+//     console.log('Theheader got clicked');
+// })
 
-document.body.addEventListener('click', (stuff) => {
-    console.log(stuff);
-    console.log('the body of the page got clicked');
-})
+// document.body.addEventListener('click', (stuff) => {
+//     console.log(stuff);
+//     console.log('the body of the page got clicked');
+// })
+
+let bodyColor = document.querySelector("body")
+
+bodyColor.addEventListener("keyup", event => event
+.target.style.backgroundColor = "hotpink")
+
+
+let pColor = document.querySelector("p")
+
+pColor.addEventListener("dblclick", event => event
+.target.style.color = "purple")
+
+let logo = document.querySelector("h1.logo-heading");
+logo.addEventListener("wheel", event => event 
+.target.style.color = "blue")
+
+
+
