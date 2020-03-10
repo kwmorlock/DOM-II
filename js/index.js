afde9a505f6d19b520353b8aadc8ff6121b315af
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-10 14:47:41 -0400

    first push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+homelink.addEvenListener(
+    'click',
+    () => {
+
+        console.log('the link got clicked');
+    }
+)
+
+document.querySelector('nav').addEventListener('click', () => {
+    console.log('the nav got clicked');
+})
+
+document.querySelector('header').addEventListener('click', () => {
+    console.log('Theheader got clicked');
+})
+
+document.body.addEventListener('click', (stuff) => {
+    console.log(stuff);
+    console.log('the body of the page got clicked');
+})
