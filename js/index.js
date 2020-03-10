dc1e748d99ca4ddf1e915be4c0e0bfd5aea3fe84
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-10 16:42:10 -0400

    10 done

--- js/index.js
+++ js/index.js
@@ -21,10 +21,14 @@
 //     console.log('the body of the page got clicked');
 // })
 
+
+//first
 let bodyColor = document.querySelector("body")
 
 bodyColor.addEventListener("keyup", event => event
 .target.style.backgroundColor = "hotpink")
+
+//second
 
 
 let pColor = document.querySelector("p")
@@ -32,11 +36,28 @@ let pColor = document.querySelector("p")
 pColor.addEventListener("dblclick", event => event
 .target.style.color = "purple")
 
+//third
+
+let pColors = document.querySelector("p")
+
+pColors.addEventListener("click", event => event
+.target.style.backgroundColor = "orange")
+
+//fourth
+
+
+let logos = document.querySelector("h1.logo-heading");
+logos.addEventListener("dblclick", event => event 
+.target.style.backgroundColor = "red")
+
+//fifth
+
 
 let logo = document.querySelector("h1.logo-heading");
 logo.addEventListener("wheel", event => event 
 .target.style.color = "blue")
 
+//sixth
 
 let mainImage = document.querySelector(".intro img")
 this.addEventListener("drag", event => {
@@ -44,10 +65,29 @@ this.addEventListener("drag", event => {
     mainImage.setAttribute("src", "img/boom.png")
     })
 
-
+//seventh
 
     let destinationColor = document.querySelector("h4");
 destinationColor.addEventListener("mouseover", event => event 
 .target.style.color = "lightblue")
+
+// eighth 
+
+
+let bColor = document.querySelector("h4");
+bColor.addEventListener("mousemove", event => event 
+.target.style.backgroundColor = "green")
+
+//ninth
+
+let hColor = document.querySelector("h2");
+hColor.addEventListener("mousemove", event => event 
+.target.style.backgroundColor = "black")
+
+//tenth
+
+let haColor = document.querySelector("h2");
+haColor.addEventListener("mouseup", event => event 
+.target.style.color = "white")
 
 
